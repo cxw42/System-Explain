e8e53ed91ebfb9c74a302dd61c9a68b3518a5896
Chris White <cxwembedded@gmail.com>
2019-10-25 21:45:45 -0400

    Initial commit from minil

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+System::Explain - It's new $module
+
+# SYNOPSIS
+
+    use System::Explain;
+
+# DESCRIPTION
+
+System::Explain is ...
+
+# LICENSE
+
+Copyright (C) Chris White.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Chris White <cxwembedded@gmail.com>
---
+++ lib/System/Explain.pm
@@ -0,0 +1,39 @@
+package System::Explain;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+System::Explain - It's new $module
+
+=head1 SYNOPSIS
+
+    use System::Explain;
+
+=head1 DESCRIPTION
+
+System::Explain is ...
+
+=head1 LICENSE
+
+Copyright (C) Chris White.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Chris White E<lt>cxwembedded@gmail.comE<gt>
+
+=cut
+
